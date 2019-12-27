46405a5b8e47867a09835a10b3db7a7366e2a0a8
Romell Pineda <romell.pineda@gmail.com>
2019-12-26 20:41:52 -0800

    folder setup

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
+'use strict';
 
+require('dotenv').config();
+
+const express = require('express');
+const ejs = require('ejs');
+
+// const pg = require('pg');
+// const superagent = require('superagent');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('pages/index')
+});
+
+app.listen(PORT, () => console.log(`app running on ${PORT}`));
