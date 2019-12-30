58cc035abe1510d45cb4266495bac45b5082f3be
Mattäus S. Isenberg <mattaus@gmx.com>
2019-12-30 14:53:38 -0800

    commit of functional API call

--- server.js
+++ server.js
@@ -30,6 +30,22 @@ app.get('/main', (req, res) => {
 app.get('/one', (req, res) => {
   res.render('pages/form');
 });
+app.post('/one', new_Activity_Search);
+
+app.get('/two', (req, res) => {
+  res.render('pages/form');
+});
+app.post('/two', new_Activity_Search);
+
+app.get('/three', (req, res) => {
+  res.render('pages/form');
+});
+app.post('/three', new_Activity_Search);
+
+app.get('/four', (req, res) => {
+  res.render('pages/form');
+});
+app.post('/four', new_Activity_Search);
 
 
 app.get('/', (req, res) => {
@@ -45,6 +61,56 @@ app.get('/', (req, res) => {
     }
   });
 });
+
+
+function Activity(object){
+  //console.log(object);
+  this.activity = object.activity;
+  this.accessibility = object.accessibility;
+  this.type = object.type;
+  this.participants = object.participants;
+  this.price = object.price;
+  this.link = object.link;
+  this.key = object.key;
+}
+
+
+function new_Activity_Search(request, response){
+  let request_parameters = request.body;
+  var activities_Array = [];
+  let promisesArray = [];
+
+  let url = `https://www.boredapi.com/api/activity?price=${request_parameters.price}&participants=${request_parameters.participants}`;
+
+  function promiseConstructor(url){
+
+    return new Promise( (resolve, reject) => {
+      superagent.get(url).then(result => {
+      let new_Activity = new Activity(result.body);
+      console.log('HEY NEW ACTIVITY', new_Activity);
+      resolve(new_Activity)
+      // console.log('EXPRESS ARRAY',activities_Array);
+    }).catch( err => {
+      console.error(err);
+    });
+    })
+
+  }
+
+  for(let i = 0; i < 5; i++){
+
+    let newPromise =  promiseConstructor(url)
+    promisesArray.push(newPromise)
+
+  }
+  
+  console.log('PROMISESARRAY', promisesArray);
+  Promise.all(promisesArray).then( (superAgentResponses) => {
+    response.render('./pages/detail', {activity_Listings:superAgentResponses});
+  });
+
+}
+
 
 
 
