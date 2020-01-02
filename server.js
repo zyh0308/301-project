8965e6c9403bc86346e68b60fd12c299a5868b65
petewhartnett <petewhartnett@outlook.com>
2020-01-02 14:57:54 -0800

    no changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const methodoverride = require('method-override');
 const pg = require('pg');
 const superagent = require('superagent');
 
-const PORT = process.env.PORT || 3470; 
+const PORT = process.env.PORT || 3484; 
 const app = express();
 
 const expressLayouts=require('express-ejs-layouts');
